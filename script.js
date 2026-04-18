62130d783d1ea92e354dd9c0844b50f79454475c
monu8208 <monu8208@gmail.com>
2026-04-18 17:01:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function generateAudio() {
-  alert("Button working ✅");
-}  }
+async function generateAudio() {
+  const text = document.getElementById("text").value;
+
+  if (!text) {
+    alert("Text likho pehle");
+    return;
+  }
+
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  // voice settings (optional but better)
+  utterance.rate = 1;
+  utterance.pitch = 1;
+  utterance.lang = "en-US";
+
+  speechSynthesis.speak(utterance);
 }
